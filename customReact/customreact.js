c09bd32f634f730abf06a781c8430968a333e0a4
Aman Shahi <blockscoding650@gmail.com>
2024-10-03 18:45:20 +0530

    updated

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,9 +1,16 @@
 function customRender(reactElement, mainContainer){
-    const domElement = document.createElement(reactElement.type);
-    domElement.innerHtml = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
 
+    // const domElement = document.createElement(reactElement.type);
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     mainContainer.appendChild(domElement)
 }
 
